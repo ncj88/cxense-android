61ea949d8469c17dcb23f34cfbed246610748cf9
Dmitriy <dekanszn@gmail.com>
2019-04-03 11:20:33 +0400

    Update ExternalUserId constraints

--- sdk/src/main/java/com/cxense/cxensesdk/model/ExternalUserId.java
+++ sdk/src/main/java/com/cxense/cxensesdk/model/ExternalUserId.java
@@ -12,12 +12,12 @@ public final class ExternalUserId extends KeyValueParameter<String, String> {
      * @param userType external user type
      * @param userId   external user id
      * @throws IllegalArgumentException if external user type has a length less than 1 or greater than 10 characters,
-     *                                  if external user id has a length less than 1 or greater than 40 characters.
+     *                                  if external user id has a length less than 1 or greater than 64 characters.
      */
     ExternalUserId(String userType, String userId) {
         super();
         Preconditions.checkStringNotNullMaxLength(userType, "userType", 10);
-        Preconditions.checkStringNotNullMaxLength(userId, "userId", 40);
+        Preconditions.checkStringForRegex(userId, "userId", "^[\\w =@+-\\.]{1,64}$", "The valid characters are digits, letters, space and the special characters \"=\", \"@\", \"+\", \"-\", \"_\" and \".\". Max length is 64 symbols");
         key = userType;
         value = userId;
     }
