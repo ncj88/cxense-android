29ea3864785a92cf7c683fd972a4ec1c26a79d8b
Dmitriy <dekanszn@gmail.com>
2019-04-05 14:49:23 +0400

    Fix release script

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git checkout master && ./gradlew clean release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.forceVersion=$1 && git checkout develop && git merge master && git push && VERSION=`./gradlew cV -q -Prelease.quiet` && sleep 60 && curl https://jitpack.io/com/cxpublic/cxense-android/$VERSION/cxense-android-$VERSION.pom
+git checkout master && git pull && ./gradlew clean release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.forceVersion=$1 && git checkout develop && git merge master && git push && VERSION=`./gradlew cV -q -Prelease.quiet` && sleep 60 && curl https://jitpack.io/com/cxpublic/cxense-android/$VERSION/cxense-android-$VERSION.pom
