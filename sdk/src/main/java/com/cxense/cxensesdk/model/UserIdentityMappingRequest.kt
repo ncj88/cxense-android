296e95a2cc2253eceb133cc0a6eae9444388ab70
Dmitriy <dekanszn@gmail.com>
2019-12-19 12:22:43 +0400

    Fix request parameter name

--- sdk/src/main/java/com/cxense/cxensesdk/model/UserIdentityMappingRequest.kt
+++ sdk/src/main/java/com/cxense/cxensesdk/model/UserIdentityMappingRequest.kt
@@ -7,7 +7,7 @@ import com.google.gson.annotations.SerializedName
  *
  */
 class UserIdentityMappingRequest(
-    @SerializedName("cxenseId") val cxenseId: String,
+    @SerializedName("cxid") val cxenseId: String,
     @SerializedName("type") val type: String,
     @SerializedName("id") val id: String? = null
 )
